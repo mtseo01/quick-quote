92ae9a3514f30988dad0bbcfd234f7a7e580019d
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-16 10:03:56 +0900

    Add check-auth middleware

--- back/api/routes/client.js
+++ back/api/routes/client.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/client');
+const checkAuth = require('../middleware/check-auth');
 
-router.post('/', clientController.createClient);
-router.get('/:createrId', clientController.getClientAll);
+router.post('/', checkAuth, clientController.createClient);
+router.get('/:createrId', checkAuth, clientController.getClientAll);
 
 module.exports = router;
--- back/api/routes/user.js
+++ back/api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
-router.get('/:userId', userController.getUserInfo);
+router.get('/:userId', checkAuth, userController.getUserInfo);
 
 module.exports = router;
