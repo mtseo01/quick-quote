2e80e686088f82dc9cd58a6fcaadcaa89b33d523
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-16 10:00:49 +0900

    #1 Feat: Check Auth using token (middleware)

---
+++ back/api/middleware/check-auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+	const { token } = req.cookies;
+	if (!token) {
+		return res.status(401).json({
+			success: false,
+			message: '권한 없음. (토큰 없음)',
+		});
+	} else {
+		try {
+			jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
+				if (error) {
+					return res.status(403).json({
+						success: false,
+						message: '유효한 토큰이 아닙니다.',
+						error,
+					});
+				} else {
+					req.userData = decoded;
+					next();
+				}
+			});
+		} catch (error) {
+			return res
+				.stauts(500)
+				.json({ success: false, message: '권한이 없습니다.' });
+		}
+	}
+};
