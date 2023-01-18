95ae00af72cbdfbddb45a30606ee580de929e0df
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-18 22:34:50 +0900

    #4 Feat: Client Page & Create Client Page

---
+++ front/src/views/ClientPage.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="container">
+		<!-- <div class="nav">
+			<p>목록</p>
+			<p class="p-btn" @click="post">거래처 정보 입력</p>
+			<p class="p-btn" @click="get">거래처 조회</p>
+		</div>
+		<div v-if="clientPost" class="component"><CreateForm></CreateForm></div> -->
+		<CreateForm></CreateForm>
+	</div>
+</template>
+<script>
+import CreateForm from '@/components/client/CreateForm.vue';
+export default {
+	components: { CreateForm },
+	data() {
+		return {
+			// clientPost: false,
+			// clinetRead: false,
+		};
+	},
+	setup() {},
+	created() {},
+	mounted() {},
+	unmounted() {},
+	methods: {
+		// post() {
+		// 	this.clinetRead = false;
+		// 	return (this.clientPost = true);
+		// },
+		// get() {
+		// 	this.clientPost = false;
+		// 	return (this.clinetRead = true);
+		// },
+	},
+};
+</script>
+
+<style scoped>
+/* .container {
+	display: flex;
+}
+.nav {
+	position: relative;
+	left: 0;
+	width: 200px;
+}
+.p-btn:hover {
+	cursor: pointer;
+} */
+</style>
