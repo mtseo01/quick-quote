76f2fc34582579b3a7b8e0e487698665b2738009
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-19 20:53:12 +0900

    Fix: Modify API's resource name

--- front/src/api/user.js
+++ front/src/api/user.js
@@ -2,17 +2,17 @@ import { instance } from './index';
 
 // 회원가입 API
 function registerUser(userObj) {
-	return instance.post('user/signup', userObj);
+	return instance.post('users/signup', userObj);
 }
 
 // 로그인 API
 function loginUser(userObj) {
-	return instance.post('user/login', userObj, { withCredentials: true });
+	return instance.post('users/login', userObj, { withCredentials: true });
 }
 
 // 유저정보 조회 API
 function getUserInfo() {
-	return instance.get('user/information', { withCredentials: true });
+	return instance.get('users/information', { withCredentials: true });
 }
 
 export { registerUser, loginUser, getUserInfo };
