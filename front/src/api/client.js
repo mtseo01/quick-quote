aefcd35fff5a788aae7091f7dae08bc5fc0ae276
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-19 21:01:59 +0900

    #4 Feat: Get Client & Update Client API (front-ent)

--- front/src/api/client.js
+++ front/src/api/client.js
@@ -2,11 +2,24 @@ import { instance } from './index';
 
 // 거래처 생성 API
 function registerClient(clientObj) {
-	return instance.post('client', clientObj, { withCredentials: true });
+	return instance.post('clients', clientObj, { withCredentials: true });
 }
 
+// 모든 거래처 조회 API
 function getClientAll() {
-	return instance.get('client', { withCredentials: true });
+	return instance.get('clients', { withCredentials: true });
 }
 
-export { registerClient, getClientAll };
+// 특정 거래처 조회 API
+function getClinet(clientId) {
+	return instance.get('clients/' + clientId, { withCredentials: true });
+}
+
+// 거래처 정보 수정 API
+function updateClient(clientId, clientObj) {
+	return instance.put('clients/' + clientId, clientObj, {
+		withCredentials: true,
+	});
+}
+
+export { registerClient, getClientAll, getClinet, updateClient };
