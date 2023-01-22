b3868c2696652ff49e4a537989f84b77154078c3
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-22 22:21:38 +0900

    #8 Feat: Quotation UserForm & ClientForm Components (front-end)

---
+++ front/src/components/quotation/ClientForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <form @submit.prevent>
+    <div>
+      <span>수신인</span>
+      <button @click="sendClient">검색</button>
+    </div>
+    <div>
+      <input
+        @input="sendData"
+        placeholder="상호명(*필수)"
+        type="text"
+        v-model="client.companyName"
+      />
+    </div>
+    <div>
+      <input
+        placeholder="담당자 이름"
+        type="text"
+        v-model="client.clientName"
+      />
+    </div>
+    <div>
+      <input
+        placeholder="사업자등록번호"
+        type="text"
+        v-model="client.companyRegiNum"
+      />
+    </div>
+    <div><input placeholder="이메일" type="text" v-model="client.email" /></div>
+    <div>
+      <input placeholder="전화번호" type="text" v-model="client.telephoneNum" />
+    </div>
+    <div>
+      <input placeholder="주소" type="text" v-model="client.companyAddress" />
+    </div>
+  </form>
+</template>
+<script>
+// import { getClinet } from '@/api/client';
+export default {
+  components: {},
+  data() {
+    return {
+      client: {
+        companyName: '',
+        clientName: '',
+        companyRegiNum: '',
+        email: '',
+        telephoneNum: '',
+        companyAddress: '',
+        _id: '',
+      },
+
+      // logMessage: '',
+    };
+  },
+  computed: {
+    sendData() {
+      return this.$emit('client-data', this.client);
+    },
+  },
+  setup() {},
+  created() {
+    // this.fetch();
+  },
+  mounted() {},
+  unmounted() {},
+  methods: {
+    // async fetch() {
+    //   try {
+    //     const clientId = this.$route.params.id;
+    //     const { data } = await getClinet(clientId);
+    //     this.client = data.doc;
+    //     this.logMessage = data.message;
+    //   } catch (error) {
+    //     this.logMessage = error.response.data.message;
+    //   }
+    // },
+  },
+};
+</script>
---
+++ front/src/components/quotation/UserForm.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div class="container-user">
+      <table>
+        <tr>
+          <td rowspan="6">공<br />급<br />자</td>
+          <td>사업자등록번호</td>
+          <td colspan="3">
+            <input id="regi-num" type="text" v-model="user.companyRegiNum" />
+            <button @click="getUserInfo">유저 불러오기</button>
+          </td>
+        </tr>
+        <tr>
+          <td>상호</td>
+          <td colspan="3">
+            <input id="company-name" type="text" v-model="user.companyName" />
+          </td>
+        </tr>
+        <tr>
+          <td>주소</td>
+          <td colspan="3">
+            <input id="address" type="text" v-model="user.companyAddress" />
+          </td>
+        </tr>
+        <tr>
+          <td>업태</td>
+          <td>
+            <input id="business-type" type="text" v-model="user.businessType" />
+          </td>
+          <td>업종</td>
+          <td>
+            <input id="business-item" type="text" v-model="user.businessItem" />
+          </td>
+        </tr>
+        <tr>
+          <td>담당자</td>
+          <td><input id="name" type="text" v-model="user.userName" /></td>
+          <td>전화번호</td>
+          <td>
+            <input id="telephone" type="text" v-model="user.telephoneNum" />
+          </td>
+        </tr>
+        <tr>
+          <td>이메일</td>
+          <td colspan="3">
+            <input id="email" type="text" v-model="user.email" />
+            <!-- <input id="email" type="text" v-model="user.email[0]" />
+            <span>@</span>
+            <input id="email" type="text" v-model="user.email[1]" /> -->
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+import { getUserInfo } from '@/api/user';
+export default {
+  components: {},
+  data() {
+    return {
+      user: {
+        userId: '',
+        userName: '',
+        email: '',
+        companyName: '',
+        companyRegiNum: '',
+        telephoneNum: '',
+        companyAddress: '',
+        businessType: '',
+        businessItem: '',
+      },
+    };
+  },
+  computed: {
+    changeData() {
+      return this.$emit('user-data', this.user);
+    },
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    async getUserInfo() {
+      try {
+        const { data } = await getUserInfo();
+        // const email = data.user.email.split('@');
+        this.user = data.user;
+        // this.user.email = email;
+        console.log(data);
+        this.sendData();
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    },
+
+    sendData() {
+      return this.$emit('user-data', this.user);
+    },
+  },
+};
+</script>
