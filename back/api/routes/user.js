3968687abd2f3916b7ee3dc2e70d5169a1932d32
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-31 10:09:19 +0900

    Fix: HTTP method because of req.body (delete -> post)

--- back/api/routes/user.js
+++ back/api/routes/user.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const userController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
+// 회원가입 API
 router.post('/signup', userController.signup);
+
+// 로그인 API
 router.post('/login', userController.login);
+
+// 특정 유저 정보 가져오기 API
 router.get('/information', checkAuth, userController.getUserInfo);
+
+// 특정 유저 정보 수정 API
 router.put('/:userId', checkAuth, userController.updateUser);
-router.delete('/:userId', checkAuth, userController.deleteUser);
+
+// 회원 탈퇴 API
+router.post('/:userId', checkAuth, userController.deleteUser);
 
 module.exports = router;
