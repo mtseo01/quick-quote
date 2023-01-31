00a5c8ba058ba0b6bfe4c53fc8db69b3b5793cff
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-31 10:25:35 +0900

    #8 Feat: Get/ Update/ Delete Quotation API (front-end)

--- front/src/api/quotation.js
+++ front/src/api/quotation.js
@@ -5,4 +5,30 @@ function createQuotation(data) {
   return instance.post('quotations', data, { withCredentials: true });
 }
 
-export { createQuotation };
+// 전체 견적서 가져오기 API
+function getQuotationsAll() {
+  return instance.get('quotations', { withCredentials: true });
+}
+
+// 특정 견적서 가져오기 API
+function getQuotation(id) {
+  return instance.get('quotations/' + id, { withCredentials: true });
+}
+
+// 견적서 수정하기 API
+function updateQuotation(id, data) {
+  return instance.put('quotations/' + id, data, { withCredentials: true });
+}
+
+// 견적서 삭제 API
+function deleteQuotation(id) {
+  return instance.delete('quotations/' + id, { withCredentials: true });
+}
+
+export {
+  createQuotation,
+  getQuotationsAll,
+  getQuotation,
+  updateQuotation,
+  deleteQuotation,
+};
