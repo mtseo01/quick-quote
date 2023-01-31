cb07faf1c9e369107f5cceecf96f435dee31baf4
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-31 11:57:26 +0900

    #7 Feat: Update User Password Page

---
+++ front/src/components/user/EditPasswordForm.vue
@@ -0,0 +1,131 @@
+<template>
+  <div v-if="alert" class="alert-form">
+    {{ logMessage }}
+  </div>
+  <form @submit.prevent>
+    <div>
+      <label for="password">password</label>
+      <input
+        id="password"
+        autofocus
+        required
+        type="password"
+        placeholder="기존 비밀번호"
+        v-model="password"
+      />
+    </div>
+    <div>
+      <label for="new-password">new password</label>
+      <input
+        id="new-password"
+        required
+        type="password"
+        placeholder="새 비밀번호"
+        v-model="newPassword"
+      />
+    </div>
+    <div>
+      <label for="confirm-new-password">confirm new password</label>
+      <input
+        id="confirm-new-password"
+        required
+        type="password"
+        placeholder="새 비밀번호 확인"
+        v-model="confirmNewPassword"
+      />
+    </div>
+    <button @click="update()">비빌번호 변경</button>
+  </form>
+</template>
+<script>
+import { updatePassword } from '@/api/user';
+export default {
+  components: {},
+  data() {
+    return {
+      password: '',
+      newPassword: '',
+      confirmNewPassword: '',
+      alert: false,
+      logMessage: '',
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    async update() {
+      if (this.newPassword === this.confirmNewPassword) {
+        try {
+          const userId = this.$route.params.id;
+          const userObj = {
+            password: this.password,
+            newPassword: this.newPassword,
+          };
+          const res = await updatePassword(userId, userObj);
+          console.log(res);
+          this.alert = true;
+          this.logMessage = res.data.message;
+        } catch (error) {
+          console.log(error);
+          this.alert = true;
+          this.logMessage = error.response.data.message;
+        }
+      } else {
+        this.alert = true;
+        this.logMessage = '비밀번호가 틀렸습니다';
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+div .alert-form {
+  color: rgb(251, 39, 39);
+  border-radius: 8px;
+  padding: 20px 20px;
+  background: rgba(59, 65, 75, 0.836);
+  font-size: 14px;
+  margin-bottom: 8px;
+  transition: 0.3s;
+}
+
+form {
+  width: 320px;
+  padding: 20px 20px;
+  border-radius: 8px;
+  text-align: center;
+  background: rgba(59, 65, 75, 0.836);
+}
+label {
+  display: block;
+  font-size: 14px;
+  margin-bottom: 2px;
+  text-align: left;
+}
+
+input {
+  padding: 5px 12px;
+  margin-top: 4px;
+  margin-bottom: 8px;
+  border: 1px solid;
+  border-radius: 4px;
+  width: 280px;
+  height: 25px;
+  border: none;
+}
+button {
+  color: white;
+  background-color: rgb(202, 10, 64);
+  display: block;
+  border: none;
+  width: 100%;
+  padding: 5px 12px;
+  margin-top: 8px;
+  border-radius: 4px;
+}
+button:hover {
+  background-color: rgb(219, 40, 91);
+}
+</style>
---
+++ front/src/views/user/EditPasswordPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>비빌번호 변경</h1>
+  <EditPasswordForm />
+</template>
+<script>
+import EditPasswordForm from '@/components/user/EditPasswordForm.vue';
+export default {
+  components: { EditPasswordForm },
+  data() {
+    return {
+      sampleData: '',
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style scoped>
+h1 {
+  color: white;
+  text-align: center;
+  font-weight: regular;
+  margin: 10px;
+}
+</style>
