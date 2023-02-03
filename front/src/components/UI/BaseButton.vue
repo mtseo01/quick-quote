00ffe131beb45cb27b4ac5cb5500d5f2acf4e63f
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-03 12:39:59 +0900

    #12 Feat: BaseButton Global Component

---
+++ front/src/components/UI/BaseButton.vue
@@ -0,0 +1,72 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: ['mode'],
+};
+</script>
+
+<style scoped>
+button {
+  padding: 4px;
+  margin: 2px;
+  border: none;
+  border-radius: 4px;
+  font-size: 12px;
+  color: white;
+  min-height: 25px;
+  min-width: 40px;
+  cursor: pointer;
+  background-color: rgb(73, 109, 154);
+}
+button:hover {
+  background-color: rgb(111, 148, 192);
+}
+
+.comfirm {
+  background-color: rgb(73, 154, 73);
+}
+.comfirm:hover {
+  background-color: rgb(90, 172, 90);
+}
+.large-comfirm {
+  display: block;
+  padding: 5px 12px;
+  margin: 6px 0 6px;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  color: white;
+  width: 100%;
+  cursor: pointer;
+  background-color: rgb(73, 154, 73);
+}
+.large-comfirm:hover {
+  background-color: rgb(90, 172, 90);
+}
+
+.cancel {
+  background-color: rgb(202, 10, 64);
+}
+.cancel:hover {
+  background-color: rgb(219, 40, 91);
+}
+.large-cancel {
+  display: block;
+  padding: 5px 12px;
+  margin: 6px 0 6px;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  color: white;
+  width: 100%;
+  cursor: pointer;
+  background-color: rgb(202, 10, 64);
+}
+.large-cancel:hover {
+  background-color: rgb(219, 40, 91);
+}
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+// 전역 컴포넌트
+import BaseButton from '@/components/UI/BaseButton.vue';
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('base-button', BaseButton)
+  .mount('#app');
