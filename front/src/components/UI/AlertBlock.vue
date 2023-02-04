663a3866b1f7d8a46171719c85bda1c73323e683
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-04 20:07:56 +0900

    Feat: AlertBlock Global Component

---
+++ front/src/components/UI/AlertBlock.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="mode" @click="$emit('close-alert')">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ['mode'],
+  emits: ['close-alert'],
+  data() {
+    return {
+      sampleData: '',
+    };
+  },
+
+  methods: {},
+};
+</script>
+<style scoped>
+div {
+  width: 320px;
+  cursor: pointer;
+  color: white;
+  border-radius: 8px;
+  padding: 20px 20px;
+  background: rgba(59, 65, 75, 0.836);
+  font-size: 14px;
+  margin-bottom: 8px;
+  transition: 0.3s;
+}
+
+.success {
+  color: rgb(0, 97, 252);
+}
+.warning {
+  color: rgb(254, 224, 1);
+}
+.error {
+  color: rgb(251, 39, 39);
+}
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -6,10 +6,12 @@ import store from './store';
 // 전역 컴포넌트
 import BaseButton from '@/components/UI/BaseButton';
 import BaseModal from '@/components/UI/BaseModal';
+import AlertBlock from '@/components/UI/AlertBlock';
 
 createApp(App)
   .use(store)
   .use(router)
   .component('base-button', BaseButton)
   .component('base-modal', BaseModal)
+  .component('alert-block', AlertBlock)
   .mount('#app');
