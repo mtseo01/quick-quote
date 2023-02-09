35f464c11a23147295f32d8c50f4766a22bc6114
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-10 00:35:15 +0900

    Vuex of User Auth

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,9 +1,30 @@
 import { createStore } from 'vuex';
-
+import { deleteCookie, getTokenFromCookie } from '@/utils/cookie';
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    token: getTokenFromCookie() || '',
+  },
+  getters: {
+    isLogin(state) {
+      return !!state.token || getTokenFromCookie();
+    },
+    getToken(state) {
+      return state.token;
+    },
+  },
+  mutations: {
+    setToken(state) {
+      state.token = getTokenFromCookie();
+    },
+    login(state) {
+      state.isLogin = true;
+      state.token = getTokenFromCookie();
+    },
+    logout(state) {
+      state.token = '';
+      deleteCookie('token');
+    },
+  },
   actions: {},
   modules: {},
 });
