86e7733bc538290452863db2dce09cbbf7fa821a
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-26 22:37:33 +0900

    Modify Product Schema

--- back/api/models/product.js
+++ back/api/models/product.js
@@ -4,7 +4,7 @@ const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   productName: { type: String, required: true },
   unitPrice: { type: Number, required: true },
-  inventory: { type: Number, required: true },
+  category: { type: String },
   creater: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
