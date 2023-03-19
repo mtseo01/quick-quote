b33e28dbc51697c8ddfba4e4a652f2304690f041
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-19 23:45:41 +0900

    Refactor: Reuse ClientForm component (Create Client Page)

--- front/src/views/client/CreateClient.vue
+++ front/src/views/client/CreateClient.vue
@@ -1,23 +1,60 @@
 <template>
   <h1>거래처 등록</h1>
+  <alert-block v-if="alert" :mode="alertMode" @close-alert="closeAlert">
+    <p>{{ alertMessage }}</p>
+  </alert-block>
   <div>
-    <CreateForm />
+    <CreateClient fetchedData="" @form-data="create" buttonName="생성" />
   </div>
 </template>
 <script>
-import CreateForm from '@/components/client/CreateForm.vue';
+import { registerClient } from '@/api/client';
+import CreateClient from '@/components/client/ClientForm.vue';
 export default {
-  components: { CreateForm },
+  components: { CreateClient },
   data() {
     return {
-      sampleData: '',
+      alertMessage: '',
+      alert: false,
+      alertMode: null,
     };
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    async create(dataObj) {
+      try {
+        const res = await registerClient(dataObj);
+
+        console.log(res);
+        const alertObj = {
+          alertMessage: res.data.message,
+          alertMode: 'success',
+        };
+        this.setAlert(alertObj);
+      } catch (error) {
+        console.log(error.response.data.message);
+        const alertObj = {
+          alertMessage: error.response.data.message,
+          alertMode: 'error',
+        };
+        this.setAlert(alertObj);
+      }
+    },
+
+    closeAlert() {
+      this.alert = false;
+      this.alertMessage = '';
+      this.alertMode = null;
+    },
+    setAlert(alertObj) {
+      this.alertMessage = alertObj.alertMessage;
+      this.alertMode = alertObj.alertMode;
+      this.alert = true;
+    },
+  },
 };
 </script>
 <style scoped>
