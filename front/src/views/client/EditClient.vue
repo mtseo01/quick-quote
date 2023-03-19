c45072c02e548eb53a55f79f60cb4029a9b36ad1
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-19 23:46:14 +0900

    Refactor: Reuse ClientForm component (Edit Client Page)

--- front/src/views/client/EditClient.vue
+++ front/src/views/client/EditClient.vue
@@ -1,23 +1,87 @@
 <template>
   <h1>거래처 정보 변경</h1>
+  <alert-block v-if="alert" :mode="alertMode" @close-alert="closeAlert">
+    <p>{{ alertMessage }}</p>
+  </alert-block>
   <div>
-    <EditClient />
+    <ClientForm
+      v-if="fetched"
+      :fetchedData="fetchedData"
+      buttonName="수정"
+      @form-data="update"
+    />
   </div>
 </template>
 <script>
-import EditClient from '@/components/client/EditForm.vue';
+import ClientForm from '@/components/client/ClientForm.vue';
+import { getClinet, updateClient } from '@/api/client';
 export default {
-  components: { EditClient },
+  components: { ClientForm },
   data() {
     return {
-      sampleData: '',
+      alertMessage: '',
+      alert: false,
+      alertMode: null,
+      fetchedData: {},
+      fetched: false,
+      clientId: this.$route.params.id,
     };
   },
   setup() {},
   created() {},
-  mounted() {},
+  mounted() {
+    this.fetchData();
+  },
   unmounted() {},
-  methods: {},
+  methods: {
+    async fetchData() {
+      try {
+        const res = await getClinet(this.clientId);
+        this.fetchedData = res.data.doc;
+        this.fetched = true;
+
+        const alertObj = {
+          alertMessage: res.data.message,
+          alertMode: 'success',
+        };
+        this.setAlert(alertObj);
+      } catch (error) {
+        const alertObj = {
+          alertMessage: error.response.data.message,
+          alertMode: 'error',
+        };
+        this.setAlert(alertObj);
+      }
+    },
+    async update(dataObj) {
+      try {
+        const res = await updateClient(this.clientId, dataObj);
+        const alertObj = {
+          alertMessage: res.data.message,
+          alertMode: 'success',
+        };
+        this.setAlert(alertObj);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+        const alertObj = {
+          alertMessage: error.response.data.message,
+          alertMode: 'error',
+        };
+        this.setAlert(alertObj);
+      }
+    },
+    closeAlert() {
+      this.alert = false;
+      this.alertMessage = '';
+      this.alertMode = null;
+    },
+    setAlert(alertObj) {
+      this.alertMessage = alertObj.alertMessage;
+      this.alertMode = alertObj.alertMode;
+      this.alert = true;
+    },
+  },
 };
 </script>
 <style scoped>
